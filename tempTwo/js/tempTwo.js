758bdbe70ac9f13588f9a9478c53719cb2726596
Jr1213 <mohammed.jr.hammad@gmail.com>
2020-06-15 08:02:57 +0200

    finish html and css

--- tempTwo/js/tempTwo.js
+++ tempTwo/js/tempTwo.js
@@ -1,1 +1,24 @@
-window.alert('llllllll')
+$(document).ready(function () {
+    $('.slider .slick').slick({
+        autoplay: true,
+        arrows: true,
+        dots: true,
+        fade: true
+    })
+    $('.phones .slick').slick({
+        autoplay: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    })
+    
+    $('.tablate  .slick').slick({
+        autoplay: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    })
+    $('.laptop  .slick').slick({
+        autoplay: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    })
+});
