250129969d9d338aaca9f21ab32fd922ad9ed3e1
Jr1213 <mohammed.jr.hammad@gmail.com>
2020-06-15 23:25:14 +0200

    finish respisive

--- tempTwo/js/tempTwo.js
+++ tempTwo/js/tempTwo.js
@@ -8,17 +8,38 @@ $(document).ready(function () {
     $('.phones .slick').slick({
         autoplay: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        responsive: [{
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
     })
-    
+
     $('.tablate  .slick').slick({
         autoplay: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        responsive: [{
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
     })
     $('.laptop  .slick').slick({
         autoplay: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        responsive: [{
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
     })
 });
