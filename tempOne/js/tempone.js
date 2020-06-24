8a505af6438ddc82fa162527a3174c9c939da925
Jr1213 <mohammed.jr.hammad@gmail.com>
2020-06-24 21:09:32 +0200

    add all the edit functonalty

--- tempOne/js/tempone.js
+++ tempOne/js/tempone.js
@@ -3,3 +3,64 @@ $(document).ready(function () {
         autoplay: true,
     })
 });
+
+// editing template 
+ 
+let heading = $('h3');
+let pargrph = $('p');
+let headingSmall = $('h5');
+let edit = $('#edit');
+let oldText = $("#oldText");
+let closeBtn = $('.closed');
+let saveBtn = $('.save');
+let inputVal;
+
+heading.click(function () {
+    currentItem = $(this);
+    let currentText = $(this).text();
+    oldText.text(currentText)
+    edit.css('display', 'flex');
+    $('#inputVal').keyup(function () {
+        inputVal = $("#inputVal").val();
+    });
+    saveBtn.click(function () {
+        currentItem.text(inputVal);
+        $("#inputVal").val('');
+        edit.css('display', 'none');
+    });
+})
+
+headingSmall.click(function () {
+    currentItem = $(this);
+    let currentText = $(this).text();
+    oldText.text(currentText)
+    edit.css('display', 'flex');
+    $('#inputVal').keyup(function () {
+        inputVal = $("#inputVal").val();
+    });
+    saveBtn.click(function () {
+        currentItem.text(inputVal);
+        $("#inputVal").val('');
+        edit.css('display', 'none');
+    });
+})
+
+pargrph.click(function () {
+    currentItem = $(this);
+    let currentText = $(this).text();
+    oldText.text(currentText)
+    edit.css('display', 'flex');
+    $('#inputVal').keyup(function () {
+        inputVal = $("#inputVal").val();
+    });
+    saveBtn.click(function () {
+        currentItem.text(inputVal);
+        $("#inputVal").val('');
+        edit.css('display', 'none');
+    });
+})
+
+closeBtn.click(function () {
+    edit.css('display', 'none');
+})
+
